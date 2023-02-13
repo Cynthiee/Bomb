0154c1488a7a01ca37f9fbca1ae2b98358df8b6d
Cynthia Ibegbu <93955311+Cynthiee@users.noreply.github.com>
2023-02-13 17:08:59 +0100

    madeit

--- hello.c
+++ hello.c
@@ -1,1 +1,17 @@
 // This is my first C program
+/* syntax - set of rules that define the structure of a language
+    #include <stdio.h> - header file library
+    main - main function
+    printf - output function
+    \n - prints new line
+
+*/
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!\n");
+  printf("I am learning C.\n\n");
+  printf("It is awesome");
+  
+  return 0;
+}
