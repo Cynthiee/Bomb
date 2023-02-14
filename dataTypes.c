49346a86485773a4d122b3040a0f13d54dd076db
Cynthia Ibegbu <93955311+Cynthiee@users.noreply.github.com>
2023-02-14 09:53:26 +0100

    precision

--- dataTypes.c
+++ dataTypes.c
@@ -1,8 +1,27 @@
 /*
-
+Data types in C refer to an extensive system used for 
+declaring variables or functions of different types.
+The type variable determins how mcuh space it occupies
+and how the bit pattern stored is interpreted.
 */
 #include <stdio.h>
 
 int main() {
-    
+// Data types
+    int num = 19;
+    float dec = 3.5;
+    char letter = 'V';
+    double deci = 18.28;
+// format specifiers eg "%f"
+    printf("%d\n", num);
+    printf("%f\n", dec);
+    printf("%c\n", letter);
+    printf("%1f\n", deci);
+
+// Decimal Precision
+    float myFloat = 7.2;
+    double nex = 30.86;
+
+    printf("%.1f\n", myFloat);
+    printf("%.2f", nex);
 }
